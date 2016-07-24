b38f585a8e59f8d71f6f4c62952a3e5932c5d7cb
Alex <alex.lee3851@gmail.com>
2016-07-24 02:14:26 -0700

    seed file

---
+++ src/database/seed.js
@@ -0,0 +1,57 @@
+import postGres from './postgres';
+import data1 from './aug_5';
+import data2 from './aug_6';
+import data3 from './aug_7';
+import dotenv from 'dotenv';
+dotenv.config();
+
+/* eslint-disable */
+postGres.sequelize.sync({ force: true })
+  .then(() => {
+    const stages = Object.keys(data1);
+    for (let i = 0; i < stages.length; i++) {
+      const events = data1[stages[i]];
+      for (let eventItr = 0; eventItr < events.length; eventItr++) {
+        const event = events[eventItr];
+        postGres.Hypee.create({
+          name: event.name,
+          start_time: event.start,
+          end_time: event.end,
+          location: stages[i],
+          day: 1,
+        });
+      }
+    }
+  })
+  .then(() => {
+    const stages = Object.keys(data2);
+    for (let i = 0; i < stages.length; i++) {
+      const events = data2[stages[i]];
+      for (let eventItr = 0; eventItr < events.length; eventItr++) {
+        const event = events[eventItr];
+        postGres.Hypee.create({
+          name: event.name,
+          start_time: event.start,
+          end_time: event.end,
+          location: stages[i],
+          day: 2,
+        });
+      }
+    }    
+  })
+  .then(() => {
+    const stages = Object.keys(data3);
+    for (let i = 0; i < stages.length; i++) {
+      const events = data3[stages[i]];
+      for (let eventItr = 0; eventItr < events.length; eventItr++) {
+        const event = events[eventItr];
+        postGres.Hypee.create({
+          name: event.name,
+          start_time: event.start,
+          end_time: event.end,
+          location: stages[i],
+          day: 3,
+        });
+      }
+    }    
+  });
